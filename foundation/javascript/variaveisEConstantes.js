3defa899cbc0e6b325eb5aa260d4bc303fa610f8
Vitor <victor-bash2@hotmail.com>
2024-07-13 15:43:21 -0300

    variaveis e constantes

---
+++ foundation/javascript/variaveisEConstantes.js
@@ -0,0 +1,22 @@
+//variaveis tipo var
+var a = 3;
+console.log(a);
+console.log(typeof a);
+
+//variaveis tipo let
+let b = 10;
+console.log(b);
+console.log(typeof b);
+
+//variaveis tipo let não pode redeclarar no mesmo contexto mais de uma vez
+//variaveis tipo var podem
+
+//isso gera um erro
+// let c = 100;
+// let c = 900;
+
+//variaveis tipo const
+//const nao pode ter o valor alterado depois da primeira declaração
+const d = 5;
+console.log(d);
+console.log(typeof d);
