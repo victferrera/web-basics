4a8be787d540d8b19e56221917b7780974953033
Vitor <victor-bash2@hotmail.com>
2024-07-13 16:11:50 -0300

    cuidados com numeros

---
+++ foundation/javascript/tipoNumberCuidados.js
@@ -0,0 +1,4 @@
+console.log(7 / 0) //retorn infinity
+console.log('10' / 2) //estranho por que ele converte a string '10' pra number
+console.log('Show' * 2) //retorna um not a number
+console.log(0.1 + 0.7) //nao é 100% preciso
