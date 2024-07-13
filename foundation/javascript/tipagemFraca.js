698dece03404a53d011937b2e79ae6f560af6239
Vitor <victor-bash2@hotmail.com>
2024-07-13 15:58:58 -0300

    tipagem javascript

---
+++ foundation/javascript/tipagemFraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//javascript possue tipagem dinamica
+//a mesma variável pode mudar o seu tipo
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
