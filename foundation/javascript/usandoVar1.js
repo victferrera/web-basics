6f64db59d02ad45e4b9b462c7561f0a8de448170
Vitor <victor-bash2@hotmail.com>
2024-07-16 21:14:37 -0300

    escopo de var 1

---
+++ foundation/javascript/usandoVar1.js
@@ -0,0 +1,13 @@
+{ { { { var sera = 'será' } } } }
+
+console.log(sera);
+
+//escopo de variáveis
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+
+//console.log(local); Erro!!!
