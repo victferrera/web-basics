a3f3bcadd9763bd61a8c0d9f4efd63044825094f
Vitor <victor-bash2@hotmail.com>
2024-07-16 21:06:18 -0300

    exemplo funcao variavel 2

---
+++ foundation/javascript/funcao2.js
@@ -0,0 +1,10 @@
+//funcao em variavel funcao arrow com retorno implícito
+const imprimirSoma = (a, b) => a + b;
+
+console.log(imprimirSoma(1, 3));
+
+//funcao anonima
+const soma = function (a, b){console.log(a + b)}
+
+soma(5, 15);
+
