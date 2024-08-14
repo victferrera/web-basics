5dcf708c3e69feb9e3aa32560e6fa27054a231eb
Vitor Ferreira <victor-bash2@hotmail.com>
2024-08-14 01:05:56 -0300

    função iife

---
+++ foundation/javascript/functions/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> Stands for Immediately Invoke Function Expression
+
+// função que é invocada quando é definida
+// isso ajuda a fugir do escopo global já que o que foi definido na função é escopo local
+
+(function() {
+    console.log('Olá')
+})()
