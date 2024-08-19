36594c90c070683dfa38956beb62cbf260271b99
Vitor Ferreira <victor-bash2@hotmail.com>
2024-08-18 21:59:03 -0300

    exercicio 1 cod3r

---
+++ foundation/javascript/exercises/exercise_cod3r_1.js
@@ -0,0 +1,11 @@
+// 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+// multiplicação e divisão desses valores.
+
+function operacoes(a, b) {
+    console.log('Soma: ', a + b)
+    console.log('Subtração: ', a - b)
+    console.log('Multiplicação: ', a * b)
+    console.log('Divisão: ', a / b)
+}
+
+operacoes(10, 5)
